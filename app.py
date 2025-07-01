136156aba6a2a147e84d950ef264b03d6ce886c9
Ketki Kinkar <ketki.kinkar2000@gmail.com>
2025-07-01 19:56:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -257,4 +257,6 @@ def method_not_allowed(error):
     return jsonify({'error': 'Method not allowed'}), 405
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=False) 
+    import os
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=False) 
